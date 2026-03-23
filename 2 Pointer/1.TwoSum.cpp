0594203872f342b3287d5d8d636bba64458bbf21
srushti-jawahire <jawahiresrushti@gmail.com>
2026-03-24 00:26:20 +0530

    Update 1.TwoSum.cpp
    This is optimal solution where T.C=O(n). In Brute force approach we will run two loops comparing each and every pair .
    T.C in that case will be O(n^2).

--- 2 Pointer/1.TwoSum.cpp
+++ 2 Pointer/1.TwoSum.cpp
@@ -1,3 +1,16 @@
-#include <iostream>
-#
-using namespace std;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+       int left=0;
+       int right=n-1;
+       while(left<right){
+        int sum=nums[left]+nums[right];
+        if(sum==target) return {left,right};
+        else if (sum<target) left++;
+        else right--;
+       }
+return {};
+        }
+    
+};
